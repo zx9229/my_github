29a1ffd9bc22c135930ea2bd9ab6446252794d10
xun.zhang <xun.zhang@juniorchina.com>
2016-12-13 19:00:32 +0800

    add rangex

---
+++ python/ztools/othertool.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# coding=utf-8
+
+
+def rangex(start, stop, step):
+    """自定义的rangeEx,缩写为rangex"""
+    while start < stop:
+        yield start
+        start += step
+
+
+if __name__ == "__main__":
+
+    exit(0)
