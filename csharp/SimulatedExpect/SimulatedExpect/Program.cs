615d904f8090241969b5d5e3ce4b29937fed398d
zx <zx@163.com>
2017-08-21 13:18:15 +0800

    bug fix

--- csharp/SimulatedExpect/SimulatedExpect/Program.cs
+++ csharp/SimulatedExpect/SimulatedExpect/Program.cs
@@ -4,17 +4,29 @@
     {
         static void Main(string[] args)
         {
-            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
+            for (int i = 0; true && i < args.Length; ++i)//前期调试.
+            {
+                System.Console.WriteLine(string.Format("args[{0}]=[{1}]", i, args[i]));
+            }
 
             string path = "config.json";
-            if (2 <= args.Length)
-                path = args[1];
-
-            //using语句, 定义一个范围, 在范围结束时处理对象.
-            //只要离开了这个代码段, 就自动调用这个类实例的Dispose函数.
-            using (MyExpect expect = new MyExpect())
+            if (1 <= args.Length)
             {
-                expect.run(path, encoding);
+                path = args[0];
+            }
+            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                //using语句定义了一个范围, 在范围结束时, 就自动调用这个类的实例的Dispose函数.
+                using (MyExpect expect = new MyExpect())
+                {
+                    expect.run(path, encoding);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex);
             }
 
             return;
